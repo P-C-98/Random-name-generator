52e57a0999e7cf5a0c7180359e031fa2904a69b3
PaulC98 <216263701+P-C-98@users.noreply.github.com>
2025-10-21 23:27:26 +0700

    updated

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 
-function splitText() {
-    const namesArea = document.getElementById("name-input");
-    const names = namesArea.ariaValueMax;
-    const lines = names.split('\n');
+let myDataArray = [];
 
-    
+function addItemToArray() {
+    const inputNames = document.getElementById("name-input");
+    const nameValue = inputNames.value.trim();
+    const outputList = document.getElementById("outputList")
+
+    if (nameValue !=='') {
+        myDataArray.push(nameValue);
+        inputNames.value = '';
+
+        localStorage.setItem('names', JSON.stringify(myDataArray));
+
+        displayArray();
+
+    }else {
+        alert('Please enter a value');
+    }
+
+    function goToOutputPage() {
+      window.location.href = "output.html";
+    }
 }
+
+function displayArray() {
+    const outputList = document.getElementById("outputList");
+    outputList.innerHTML = '';
+
+    myDataArray.forEach(name => {
+        const li = document.createElement("li");
+        li.textContent = name;
+        outputList.appendChild(li);
+    });
+}
+function goToOutputPage() {
+    window.location.href = "output.html";
+}
+
+
+
+
+
+
+
